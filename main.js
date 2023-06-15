b504cda95dd03f96327c5a636881546b08505807
Noxik <64279796+Noxik@users.noreply.github.com>
2023-06-15 21:30:24 +0200

    start

---
+++ main.js
@@ -0,0 +1,15 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read
+    }
+}
+
+Book.prototype.randomNumber = function() {
+    console.log(Math.random())
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet")
